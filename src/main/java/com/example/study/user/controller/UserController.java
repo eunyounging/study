c7fbbc40653a93711f994e85a44f9a18542f36c7
“eunyoung” <jyy714@gmail.com>
2024-11-30 20:09:44 +0900

    Spring Controller

---
+++ src/main/java/com/example/study/user/controller/UserController.java
@@ -0,0 +1,38 @@
+package com.example.study.user.controller;
+
+import com.example.study.user.entity.User;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class UserController {
+
+    List<User> userList = new ArrayList<>();
+
+    @GetMapping("/user/{userId}")
+    public User readUser(@PathVariable Integer userId) {
+        return this.userList.get(userId);
+    }
+
+    @GetMapping("/user")
+    public List<User> readUser() {
+        return userList;
+    }
+
+    @PostMapping("/user")
+    public User createUser(@RequestBody User user) {
+        this.userList.add(user);
+
+        return user;
+    }
+
+    @PutMapping("/user/{userId}")
+    public User updateUser(@RequestBody User user, @PathVariable Integer userId) {
+        this.userList.remove(userId.intValue());
+        this.userList.add(userId, user);
+
+        return this.userList.get(userId.intValue());
+    }
+}
---
+++ src/main/java/com/example/study/user/entity/User.java
@@ -0,0 +1,9 @@
+package com.example.study.user.entity;
+
+import lombok.Data;
+
+@Data
+public class User {
+    String name;
+    String age;
+}
